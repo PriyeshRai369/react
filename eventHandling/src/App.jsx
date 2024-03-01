ebbfe5f7cc89be156907abc7311b2a9e2e173ca7
PriyeshRai369 <priyeshrai369@gmail.com>
2024-03-01 10:36:49 +0530

    event handling

--- eventHandling/src/App.jsx
+++ eventHandling/src/App.jsx
@@ -2,13 +2,28 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mouseOver, setMouseOver] = useState(false)
+  function handelMouseOver(){
+    setMouseOver(true)
+  }
+  function handelMouseOut(){
+    setMouseOver(false)
+  }
 
   return (
-    <>
-      <h1>Priyesh</h1>
-    </>
-  )
+
+    <div className="container">
+      <h1>Hello</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button 
+      onMouseOver={handelMouseOver}
+      onMouseOut={handelMouseOut}
+      style={{backgroundColor: mouseOver ? "black" : "white" }}
+      >
+        Submit
+      </button>
+    </div>
+  );
 }
 
 export default App
